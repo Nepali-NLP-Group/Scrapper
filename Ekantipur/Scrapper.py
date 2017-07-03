856eda3aff01085927de616b700220d003e108a9
IngrojShrestha <ing.stha@gmail.com>
2017-07-03 12:30:07 +0545

    Intial Commit

---
+++ Ekantipur/Scrapper.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+import sys
+import bs4 as bs
+import urllib
+import re
+
+
+class ReadOnlyClass(type):
+    def __setattr__(self, name, value):
+        raise ValueError(name)
+
+
+class Scrapper:
+    __metaclass__ = ReadOnlyClass
+
+    def __init__(self, news_link, source):
+        self.NEWS_LINK = news_link
+        self.SOURCE = source
+        self.title = ''
+        self.body = ''
+
+    def extractContent(self):
+        r = urllib.urlopen(self.NEWS_LINK).read()
+        soup = bs.BeautifulSoup(r)
+        return self.parseContent(soup)
+
+    def parseContent(self, content):
+        print ("====================== DISPLAYING THE CONTENTS ======================")
+        self.extractCategory(content)
+
+    def extractCategory(self, content):
+        categoryOriginal = {}
+        categories = {}
+
+        for paragraph in content.find_all('a'):
+            link = paragraph.get('href')
+
+            if link is not None:
+
+                match = re.match(r'/category/', link, re.M | re.I)
+                if match:
+                    categoryOriginal[paragraph.get('href').replace("/category/", "")] = paragraph.text
+
+        # {category , category_text}
+        #remove redundant category
+        for key, value in categoryOriginal.items():
+            if key not in categories.keys():
+                categories[key] = value
+
+        self.extractHeadline(categories)
+
+    # retrieve headline for each category
+    def extractHeadline(self, categoryList):
+
+        for category, category_value in categoryList.items():
+            url = ''
+            url = url.join((self.NEWS_LINK, '/category/', category))
+
+            print (
+            "################################### Category : %s ###################################" % (category_value))
+            r = urllib.urlopen(url).read()
+            soup = bs.BeautifulSoup(r)
+
+            headlineList = []
+            for data in soup.find_all('div', {'class': ['item-wrap', 'wrap']}):
+                if data.find('a') is not None:
+                    # verify that it is content page
+                    if 'html' in data.find('a').get('href'):
+                        headlineList.append(data.find('a').get('href'))
+
+            self.newsContents(headlineList)
+
+    # retrieve the body for each headline
+    def newsContents(self, headlineList):
+
+        for index, headline in enumerate(headlineList):
+            print ("*********************************** Headline : %s **********************************" % (index + 1))
+            url = ''
+            url = url.join((self.NEWS_LINK, headline.encode('ascii', 'ignore')))
+            r = urllib.urlopen(url).read()
+            soup = bs.BeautifulSoup(r)
+
+            title_source = soup.find_all('div', {'class': ['wrap', 'content-wrapper', 'maincontent']})
+
+            for title_ in title_source:
+                if title_.find({'h1', 'h2'}) is not None:
+                    self.title = title_.find({'h1', 'h2'}).text
+
+            body_content = soup.find('div', {'class': 'content-wrapper'})
+            print ("Title : " + self.title)
+
+            for body in body_content.find_all('p'):
+                # check if it the body is empty
+                # exclude the javascript inside <p></p> tag
+                if str(body.text.encode('ascii', 'ignore'))!=""  and 'script' not in str(body):
+                    print body.text
+                    self.body +=body.text
+            print ("Body : " + self.body)
+
+def main():
+    news_link = sys.argv[1]
+    news_source_name = sys.argv[2]
+    Scrapper(news_link=news_link, source=news_source_name).extractContent()
+
+
+if __name__ == '__main__':
+    main()
---
+++ README.md
@@ -0,0 +1,43 @@
+# Scrapper
+
+Developed as a part of #Icodeformyभाषा project of Nepali NLP Group
+
+## Dependencies
+
+	Beautiful Soup , re, urllib
+
+    Install pip
+    $ sudo apt-get install python-pip
+
+
+    Installing dependencies
+
+    $ pip install beautifulsoup4
+    $ pip install urllib
+
+    or
+
+    $ pip install -r requirements.txt
+
+
+### E-Kantipur
+
+	Script to scrap Nepali news from various categories of following newspaper
+
+    1. कान्तिपुर
+    2. नारी
+    3. साप्ताहिक
+    4. नेपाल
+
+
+### Running Script
+
+You can save this code as scrapetest.py and run it in your terminal using the command:
+
+$ python Scrapper.py http://kantipur.ekantipur.com कान्तिपुर
+
+Note that if you also have Python 2.x installed on your machine, you may need to
+explicitly call Python 3.x by running the command this way:
+
+$ python3 Scrapper.py http://kantipur.ekantipur.com कान्तिपुर
+
